328b6402e7d66a96eb4180283eee7496bd5e9e51
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2025-05-24 11:43:41 +0800

    Update: Added level progression in each scene via button
    Update: Modified scene

---
+++ Assets/Scripts/SceneManager/NextLevelButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelButton : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if next scene exists in build settings
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("This is the last level!");
+        }
+    }
+}
