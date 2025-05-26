a7de2cdc1603b6dfdb3e59ba16af6f72e00a0b58
Glen Andrew <gcbulaong@up.edu.ph>
2025-05-26 22:38:46 +0800

    feat: equip power-up

---
+++ Assets/Scripts/Player/PlayerPowerUp.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerPowerUp : MonoBehaviour
+{
+    [Header("Fire Rate Properties")]
+    [SerializeField] private float fireRateIncreaseDuration = 5f;
+    [SerializeField] private float fireRateValue = 0.05f;
+
+    private PlayerStates playerStates;
+    private Coroutine fireRateCoroutine;
+
+    void Awake()
+    {
+        playerStates = GetComponent<PlayerStates>();
+    }
+
+    private void OnEnable()
+    {
+        playerStates.IncreaseFireRate += IncreaseFireRate;
+        playerStates.ReplenishHealth += ReplenishHealth;
+    }
+
+    private void OnDisable()
+    {
+        playerStates.IncreaseFireRate -= IncreaseFireRate;
+        playerStates.ReplenishHealth -= ReplenishHealth;
+    }
+
+    private void IncreaseFireRate()
+    {
+        // playerStates.FireRate = 0.05f;
+        if (fireRateCoroutine != null)
+        {
+            StopCoroutine(fireRateCoroutine);
+
+        }
+        fireRateCoroutine = StartCoroutine(FireRateTimer());
+    }
+
+    private IEnumerator FireRateTimer()
+    {
+        float originalFireRate = playerStates.FireRate;
+        playerStates.FireRate = fireRateValue;
+        yield return new WaitForSeconds(fireRateIncreaseDuration);
+        playerStates.FireRate = originalFireRate;
+        fireRateCoroutine = null;
+    }
+
+    private void ReplenishHealth()
+    {
+        if (playerStates.HealthBars < 3)
+        {
+            playerStates.HealthBars++;
+            Debug.Log("Health replenished. Current health bars: " + playerStates.HealthBars);
+        }
+
+    }
+}
