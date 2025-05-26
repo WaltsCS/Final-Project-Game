eadf43b14ef6c648492686a25ed4b2df0415fccf
Glen Andrew <gcbulaong@up.edu.ph>
2025-05-26 14:27:26 +0800

    feat: add game over text and restart button

---
+++ Assets/Scripts/SceneManager/GameManager.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Text Display")]
+    [SerializeField] private TextMeshProUGUI gameOverText;
+
+    [Header("Buttons")]
+    [SerializeField] private GameObject restartButton;
+
+    [Header("Managers")]
+    // todo: Insert managers here (time, audio, etc.)
+
+    private bool isGameActive = false;
+
+    public bool IsGameActive
+    {
+        get { return isGameActive; }
+    }
+
+    public void DisplayGameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        // Restart to the first scene
+        SceneManager.LoadScene(0);
+    }
+}
