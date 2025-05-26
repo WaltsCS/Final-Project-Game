f7748541e6f072c2f760a770c24384c0f32f3e37
Glen Andrew <gcbulaong@up.edu.ph>
2025-05-27 01:59:04 +0800

    refactor: remove unnecessary code

--- Assets/Scripts/Opps/EnemyBullet.cs
+++ Assets/Scripts/Opps/EnemyBullet.cs
@@ -5,14 +5,6 @@ public class EnemyBullet : BaseBullet
     protected override void OnCollisionEnter(Collision other)
     {
         Debug.Log($"EnemyBullet collided with: {other.gameObject.name}");
-        
-        // Check for player bullet collision
-        if (other.gameObject.CompareTag("Player Bullet"))
-        {
-            Destroy(other.gameObject);
-            Destroy(gameObject);
-            return;
-        }
 
         // Check for scene collision
         if (other.gameObject.CompareTag("Scene"))
