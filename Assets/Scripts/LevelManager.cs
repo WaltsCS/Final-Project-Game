626ecaf4b7e47e632692f91b78dc4a1bdb85952b
Glen Andrew <gcbulaong@up.edu.ph>
2025-05-27 11:29:01 +0800

    refactor: transfer methods to level manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    [Header("UI Hooks")]
-    [Tooltip("Button or UI panel to show when level is complete (Next Level)")]
-    public GameObject levelCompleteUI;
+    [Header("Text Display")]
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private TextMeshProUGUI levelCompleteText;
 
-    void Awake()
+    [Header("Buttons")]
+    [SerializeField] private GameObject restartButton;
+    [SerializeField] private GameObject nextLevelButton;
+
+    // todo: set to false in the future
+    // level starts when player clicks play in game manager
+    private bool isLevelActive = true;
+
+    public bool IsLevelActive
     {
-        if (levelCompleteUI != null)
-            levelCompleteUI.SetActive(false);
+        get { return isLevelActive; }
     }
+
 
     void Start()
     {
@@ -21,12 +30,45 @@ public class LevelManager : MonoBehaviour
 
     private IEnumerator CheckForLevelClear()
     {
+        yield return new WaitForSeconds(0.5f);
+
         // Wait until there are no more enemies in the scene
         while (GameObject.FindGameObjectsWithTag("Enemy").Length > 0)
             yield return null;
 
-        // Once clear, show your next-level UI
-        if (levelCompleteUI != null)
-            levelCompleteUI.SetActive(true);
+        DisplayLevelComplete();
+    }
+
+    private void DisplayLevelComplete()
+    {
+        isLevelActive = false;
+        levelCompleteText.gameObject.SetActive(true);
+        nextLevelButton.gameObject.SetActive(true);
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+            // UnDisplayLevelComplete();
+        }
+
+    }
+
+    public void DisplayGameOver()
+    {
+        isLevelActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        // Restart to the first scene
+        SceneManager.LoadScene(0);
     }
 }
